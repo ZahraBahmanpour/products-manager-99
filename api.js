2a351a4c09ebea282c71f7d5dacf8b60134f345c
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-20 09:46:00 +0330

    implement addToDOM

--- api.js
+++ api.js
@@ -8,8 +8,68 @@ export const readProducts = async () => {
     const res = await fetch(`${BASE_URL}/products`);
     const data = await res.json();
     const { products } = data;
-    console.log(products);
+    productsTable.innerHTML = "";
+    products.forEach(addToDOM);
   } catch (error) {
     console.log(error.message);
   }
+};
+
+// UPDATE DOM
+const addToDOM = (product) => {
+  const productRow = document.createElement("tr");
+  productRow.dataset.id = product.id;
+  const { nameCell, priceCell, countCell, createDateCell, actionCell } =
+    generateTableCells(product);
+
+  productRow.appendChild(nameCell);
+  productRow.appendChild(priceCell);
+  productRow.appendChild(countCell);
+  productRow.appendChild(createDateCell);
+  productRow.appendChild(actionCell);
+
+  productsTable.appendChild(productRow);
+};
+
+const generateTableCells = (product) => {
+  const nameCell = document.createElement("td");
+  nameCell.innerHTML = product.name;
+  nameCell.title = product.name;
+
+  const priceCell = document.createElement("td");
+  priceCell.innerHTML = product.price;
+  priceCell.title = product.price;
+
+  const countCell = document.createElement("td");
+  countCell.innerHTML = product.countInStock;
+  countCell.title = product.countInStock;
+
+  const createDateCell = document.createElement("td");
+  const date = new Date(product.createdAt).toDateString();
+  createDateCell.innerHTML = date;
+  createDateCell.title = date;
+
+  const viewButton = document.createElement("button");
+  viewButton.dataset.id = product.id;
+  viewButton.innerHTML = '<i class="bi bi-eye"></i>';
+  viewButton.title = "VIEW";
+  viewButton.className = "btn btn-warning btn-sm m-1 text-white";
+
+  const editButton = document.createElement("button");
+  editButton.dataset.id = product.id;
+  editButton.innerHTML = '<i class="bi bi-pen"></i>';
+  editButton.title = "UPDATE";
+  editButton.className = "btn btn-primary btn-sm";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.dataset.id = product.id;
+  deleteButton.innerHTML = '<i class="bi bi-trash"></i>';
+  deleteButton.title = "DELETE";
+  deleteButton.className = "btn btn-danger btn-sm m-1";
+
+  const actionCell = document.createElement("td");
+  actionCell.appendChild(viewButton);
+  actionCell.appendChild(editButton);
+  actionCell.appendChild(deleteButton);
+  return { nameCell, priceCell, countCell, createDateCell, actionCell };
 };
