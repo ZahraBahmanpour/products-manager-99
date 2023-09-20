a268ff83c2eefdc14455ea2abf4f09a8566708e7
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-09-20 10:45:30 +0330

    implement pagination onClick

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import { readProducts, controller } from "./api.js";
+
+export let currentPage = 1;
+
 // EVENT LISTENERS
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,3 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
 document.querySelector("#cancel-request").addEventListener("click", () => {
   controller.abort();
 });
+
+document
+  .querySelector("ul.pagination")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+    if (event.target.className.includes("pagination")) {
+      return;
+    }
+    const lis = document.querySelectorAll(".page-item");
+    lis.forEach((li) => li.classList.remove("active"));
+    if (event.target.parentElement.id === "previous") {
+      const page = currentPage - 1;
+      currentPage = page === 0 ? 1 : page;
+      event.currentTarget
+        .querySelector(`#page-${currentPage}`)
+        .classList.add("active");
+    } else if (event.target.parentElement.id === "next") {
+      const page = currentPage + 1;
+      currentPage =
+        page > event.target.parentElement.dataset.pageCount
+          ? event.target.parentElement.dataset.pageCount
+          : page;
+      event.currentTarget
+        .querySelector(`#page-${currentPage}`)
+        .classList.add("active");
+    } else {
+      event.target.parentElement.classList.add("active");
+      currentPage = Number(event.target.innerText);
+    }
+    await readProducts();
+    window.scrollTo({ top: document.body.scrollHeight });
+  });
--- utility.js
+++ utility.js
@@ -1,17 +1,25 @@
-const DEFAULT_PAGE_SIZE = 5;
+import { currentPage } from "./app.js";
+
+export const DEFAULT_PAGE_SIZE = 5;
 
 export const createPagination = (productCount) => {
   const pageCount = Math.ceil(productCount / DEFAULT_PAGE_SIZE);
 
-  let lis = `<li class="page-item" id="previous">
+  let lis = `<li class="page-item ${
+    Number(currentPage) === 1 ? "disabled no-events" : ""
+  }" id="previous">
                 <a class="page-link" href="#" aria-label="Previous">&laquo;</a>
               </li>`;
 
   for (let i = 1; i <= pageCount; i++) {
-    lis += `<li id="page-${i}" class="page-item"><a href="#" class="page-link">${i}</a></li>`;
+    lis += `<li id="page-${i}" class="page-item ${
+      i === Number(currentPage) ? "active" : ""
+    }"><a href="#" class="page-link">${i}</a></li>`;
   }
 
-  lis += `<li class="page-item" id="next" data-page-count="${pageCount}">
+  lis += `<li class="page-item ${
+    Number(currentPage) === pageCount ? "disabled no-events" : ""
+  }" id="next" data-page-count="${pageCount}">
               <a class="page-link" href="#" aria-label="Next">&raquo;</a>
             </li>`;
   document.querySelector("ul.pagination").innerHTML = lis;
